f232f9b37e701c37df46c45f963e08ac2c83e3a4
lucas <luca.staus@outlook.de>
2022-08-07 18:47:45 +0200

    tests for addVertex() and removeVertex()

---
+++ src/main/kotlin/TestUtils.kt
@@ -0,0 +1,28 @@
+
+import java.lang.reflect.Field
+import java.lang.reflect.Method
+
+
+fun <O, V> getFieldValue(fieldName: String, o: O, oClass: Class<O>): V {
+    val field: Field = oClass.getDeclaredField(fieldName)
+    field.isAccessible = true
+    return field.get(o) as V
+}
+
+fun <O, V> setFieldValue(fieldName: String?, o: O, oClass: Class<O>, value: V) {
+    val field: Field = oClass.getDeclaredField(fieldName)
+    field.isAccessible = true
+    field.set(o, value)
+}
+
+fun <O, V> invokeMethod(
+    methodName: String,
+    o: O,
+    oClass: Class<O>,
+    args: Array<Any?>?,
+    parameterTypes: Array<Class<*>?>
+): V {
+    val method: Method = oClass.getMethod(methodName, *parameterTypes)
+    method.isAccessible = true
+    return method.invoke(o, args) as V
+}
--- src/test/kotlin/SimpleGraphTest.kt
+++ src/test/kotlin/SimpleGraphTest.kt
@@ -1,8 +1,55 @@
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
 
 internal class SimpleGraphTest {
 
+    @BeforeEach
+    fun setup() {
+
+    }
+
     @Test
-    fun addVertex() {
+    fun `add New Vertex`() {
+        val g = SimpleGraph<Int>()
+        assertTrue { g.addVertex(1) }
+        val m = getFieldValue<SimpleGraph<Int>, HashMap<Int, MutableSet<Int>>>("m", g, g.javaClass)
+        assertContains(m, 1)
+        assertEquals(1, m.size)
+    }
+
+    @Test
+    fun `add Existing Vertex`() {
+        val g = SimpleGraph<Int>()
+        g.addVertex(1)
+        assertFalse { g.addVertex(1) }
+        val m = getFieldValue<SimpleGraph<Int>, HashMap<Int, MutableSet<Int>>>("m", g, g.javaClass)
+        assertContains(m, 1)
+        assertEquals(1, m.size)
+    }
+
+    @Test
+    fun `remove Existing Vertex`() {
+        val g = SimpleGraph<Int>()
+        g.addVertex(1)
+        assertTrue { g.removeVertex(1) }
+        val m = getFieldValue<SimpleGraph<Int>, HashMap<Int, MutableSet<Int>>>("m", g, g.javaClass)
+        assertTrue { 1 !in m }
+        assertEquals(0, m.size)
+    }
+
+    @Test
+    fun `remove vertex that is not part of the graph`() {
+        val g = SimpleGraph<Int>()
+        g.addVertex(1)
+        assertFalse { g.removeVertex(2) }
+        val m = getFieldValue<SimpleGraph<Int>, HashMap<Int, MutableSet<Int>>>("m", g, g.javaClass)
+        assertTrue { 1 in m }
+        assertTrue { 2 !in m }
+        assertEquals(1, m.size)
     }
 }
