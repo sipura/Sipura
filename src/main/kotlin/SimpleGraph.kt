ec2786749bc8d07561c248f9888c4c9b920fcce4
moritzgross <mgross321@gmail.com>
2022-08-07 19:18:33 +0200

    methods addEdge() and removeEdge()
    tests for contains and degree
    
    used @Beforeeach in tests

--- src/main/kotlin/SimpleGraph.kt
+++ src/main/kotlin/SimpleGraph.kt
@@ -1,17 +1,18 @@
 open class SimpleGraph<V> {
 
+    // If v1 and v2 are connected, then *v1 in m[v2]* and *v2 in m[v1]* has to hold
     private val m = HashMap<V, MutableSet<V>>()
 
-    operator fun contains(v: V) : Boolean = v in m
+    operator fun contains(v: V): Boolean = v in m
 
-    fun addVertex(v: V) : Boolean {
+    fun addVertex(v: V): Boolean {
         return if (v !in m) {
             m[v] = mutableSetOf()
             true
         } else false
     }
 
-    fun removeVertex(v: V) : Boolean {
+    fun removeVertex(v: V): Boolean {
         if (v !in m) return false
         for (nb in m[v]!!) {
             m[nb]!!.remove(v)
@@ -20,8 +21,30 @@ open class SimpleGraph<V> {
         return true
     }
 
-    fun degreeOf(v: V) : Int {
+    fun degreeOf(v: V): Int {
         if (v !in m) throw IllegalArgumentException("Graph does not contain the given vertex.")
         return m[v]!!.size
+    }
+
+    fun addEdge(v1: V, v2: V): Boolean {
+        if (v1 !in m) throw IllegalArgumentException("Graph does not contain v1.")
+        if (v2 !in m) throw IllegalArgumentException("Graph does not contain v2.")
+
+        if (v1 in m[v2]!!) return false
+
+        m[v1]!!.add(v2)
+        m[v2]!!.add(v1)
+        return true
+    }
+
+    fun removeEdge(v1: V, v2: V): Boolean {
+        if (v1 !in m) throw IllegalArgumentException("Graph does not contain v1.")
+        if (v2 !in m) throw IllegalArgumentException("Graph does not contain v2.")
+
+        if (v1 !in m[v2]!!) return false
+
+        m[v1]!!.remove(v2)
+        m[v2]!!.remove(v1)
+        return true
     }
 }
--- src/test/kotlin/SimpleGraphTest.kt
+++ src/test/kotlin/SimpleGraphTest.kt
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertContains
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
@@ -8,48 +9,80 @@ import kotlin.test.assertTrue
 
 internal class SimpleGraphTest {
 
+    lateinit var g: SimpleGraph<Int>
+    lateinit var m: HashMap<Int, MutableSet<Int>>
+
     @BeforeEach
     fun setup() {
-
+        g = SimpleGraph()
+        m = getFieldValue("m", g, g.javaClass)
     }
 
     @Test
     fun `add New Vertex`() {
-        val g = SimpleGraph<Int>()
         assertTrue { g.addVertex(1) }
-        val m = getFieldValue<SimpleGraph<Int>, HashMap<Int, MutableSet<Int>>>("m", g, g.javaClass)
         assertContains(m, 1)
         assertEquals(1, m.size)
     }
 
     @Test
     fun `add Existing Vertex`() {
-        val g = SimpleGraph<Int>()
         g.addVertex(1)
         assertFalse { g.addVertex(1) }
-        val m = getFieldValue<SimpleGraph<Int>, HashMap<Int, MutableSet<Int>>>("m", g, g.javaClass)
         assertContains(m, 1)
         assertEquals(1, m.size)
     }
 
     @Test
     fun `remove Existing Vertex`() {
-        val g = SimpleGraph<Int>()
         g.addVertex(1)
         assertTrue { g.removeVertex(1) }
-        val m = getFieldValue<SimpleGraph<Int>, HashMap<Int, MutableSet<Int>>>("m", g, g.javaClass)
         assertTrue { 1 !in m }
         assertEquals(0, m.size)
     }
 
     @Test
     fun `remove vertex that is not part of the graph`() {
-        val g = SimpleGraph<Int>()
         g.addVertex(1)
         assertFalse { g.removeVertex(2) }
-        val m = getFieldValue<SimpleGraph<Int>, HashMap<Int, MutableSet<Int>>>("m", g, g.javaClass)
         assertTrue { 1 in m }
         assertTrue { 2 !in m }
         assertEquals(1, m.size)
+    }
+
+    @Test
+    fun `check if vertex is contained that is not there`() {
+        assertFalse { 1 in g }
+    }
+
+    @Test
+    fun `check if vertex is contained that is present`() {
+        g.addVertex(7)
+        assertTrue{ 7 in g }
+    }
+
+    @Test
+    fun `checking degree of missing vertex throws exception`() {
+        assertThrows<IllegalArgumentException> { g.degreeOf(3) }
+    }
+
+    @Test
+    fun `degree of isolated vertex is 0`() {
+        g.addVertex(7)
+        assertEquals(0, g.degreeOf(7))
+    }
+
+    @Test
+    fun `check degrees in path of length 3`() {
+        g.addVertex(1)
+        g.addVertex(2)
+        g.addVertex(3)
+        g.addEdge(1,2)
+        g.addEdge(2,3)
+
+        assertEquals(1, g.degreeOf(1))
+        assertEquals(2, g.degreeOf(2))
+        assertEquals(1, g.degreeOf(3))
+
     }
 }
