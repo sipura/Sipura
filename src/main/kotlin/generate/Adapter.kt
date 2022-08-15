17ce1bd192c92f0a5e5e826bfec44c6008519648
moritzgross <mgross321@gmail.com>
2022-08-15 19:34:08 +0200

    created adapter for Guava-graphs

--- src/main/kotlin/generate/Adapter.kt
+++ src/main/kotlin/generate/Adapter.kt
@@ -1,5 +1,7 @@
 package generate
 
+import com.google.common.graph.GraphBuilder
+
 import graphs.SimpleGraph
 import org.jgrapht.graph.DefaultEdge
 
@@ -25,6 +27,37 @@ object Adapter {
         }
         for (edge in g.edgeSet()) {
             res.addEdge(g.getEdgeSource(edge), g.getEdgeTarget(edge))
+        }
+
+        return res
+    }
+
+    /**
+     * Most graph-stuff from Guava is marked as Beta :/
+     */
+    fun <V> toGuava(g: SimpleGraph<V>): com.google.common.graph.Graph<V> {
+        val res: com.google.common.graph.MutableGraph<V> = GraphBuilder.undirected().build()
+        for (v in g.V) {
+            res.addNode(v)
+        }
+        for ((v1, v2) in g.edgeIterator()) {
+            res.putEdge(v1, v2)
+        }
+
+        return res
+    }
+
+    /**
+     * Most graph-stuff from Guava is marked as Beta :/
+     */
+    fun <V> fromGuava(g: com.google.common.graph.Graph<V>): SimpleGraph<V> {
+        val res = SimpleGraph<V>()
+
+        for (v in g.nodes()) {
+            res.addVertex(v)
+        }
+        for (edge in g.edges()) {
+            res.addEdge(edge.nodeU(), edge.nodeV())
         }
 
         return res
