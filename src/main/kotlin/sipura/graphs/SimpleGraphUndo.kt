e535a1910f46927f1acace3762e5944f8741f437
lucas <luca.staus@outlook.de>
2023-02-04 13:07:42 +0100

    implement basic operations add new constructor add some documentation

--- src/main/kotlin/sipura/graphs/SimpleGraphUndo.kt
+++ src/main/kotlin/sipura/graphs/SimpleGraphUndo.kt
@@ -2,24 +2,78 @@ package sipura.graphs
 
 import java.util.*
 
-class SimpleGraphUndo<V> : SimpleGraph<V>() {
+/**
+ * A SimpleGraph implementation that keeps track of all [addVertex], [removeVertex], [addEdge] and [removeEdge]
+ * operations and has the option of undoing them in reverse order.
+ */
+open class SimpleGraphUndo<V>() : SimpleGraph<V>() {
 
-    private val undoStack: Stack<() -> Unit> = Stack<() -> Unit>()
+    protected val undoStack: Stack<() -> Unit> = Stack<() -> Unit>()
+
+    /**
+     * Adds all vertices and edges in [g] to this graph without adding the necessary operations to the undo stack.
+     */
+    constructor(g: SimpleGraph<V>) : this() {
+        for (v in g.V) {
+            super.addVertex(v)
+        }
+        for ((v1, v2) in g.edgeIterator()) {
+            super.addEdge(v1, v2)
+        }
+    }
+
+    /**
+     * Reverts the last operation that has been performed on this graph.
+     */
+    open fun undo() {
+        if (undoStack.isNotEmpty()) {
+            undoStack.pop()()
+        }
+    }
+
+    /**
+     * Clears the undo stack.
+     */
+    open fun clearStack() {
+        undoStack.clear()
+    }
 
     override fun addVertex(v: V): Boolean {
-        return super.addVertex(v)
+        if (super.addVertex(v)) {
+            undoStack.push { super.removeVertex(v) }
+            return true
+        }
+        return false
     }
 
     override fun removeVertex(v: V): Boolean {
-        return super.removeVertex(v)
+        val neighborSet = super.neighbors(v).toSet()
+        if (super.removeVertex(v)) {
+            undoStack.push {
+                addVertex(v)
+                for (n in neighborSet) {
+                    super.addEdge(v, n)
+                }
+            }
+            return true
+        }
+        return false
     }
 
     override fun addEdge(v1: V, v2: V): Boolean {
-        return super.addEdge(v1, v2)
+        if (super.addEdge(v1, v2)) {
+            undoStack.push { super.removeEdge(v1, v2) }
+            return true
+        }
+        return false
     }
 
     override fun removeEdge(v1: V, v2: V): Boolean {
-        return super.removeEdge(v1, v2)
+        if (super.removeEdge(v1, v2)) {
+            undoStack.push { super.addEdge(v1, v2) }
+            return true
+        }
+        return false
     }
 
     override fun copy(): SimpleGraph<V> {
