1ff498b358e859ee72cfc28a23bd0960c8c53026
Luca Staus <64088153+LucaStaus@users.noreply.github.com>
2023-02-07 13:06:50 +0100

    Undostack (#39)
    * add override methods for SimpleGraphUndo
    
    * add undo stack
    
    * implement basic operations
    add new constructor
    add some documentation
    
    * implement copy operation
    
    * switch to using a LinkedList for the undo stack and add undoSize property
    
    * add tests for SimpleGraphUndo and fix small oversight in SimpleGraphUndo.removeVertex
    
    * remove copy override in SimpleGraphUndo since the functions on the undo stack will still point to the original graph even if they are copied
    
    * fix ktlint errors
    
    * fix ktlint errors this time for real

---
+++ src/main/kotlin/sipura/graphs/SimpleGraphUndo.kt
@@ -0,0 +1,89 @@
+package sipura.graphs
+
+import java.util.LinkedList
+
+/**
+ * A SimpleGraph implementation that keeps track of all [addVertex], [removeVertex], [addEdge] and [removeEdge]
+ * operations and has the option of undoing them in reverse order.
+ */
+open class SimpleGraphUndo<V>() : SimpleGraph<V>() {
+
+    /**
+     * Returns the amount of undo operations that can be performed on this graph at this moment.
+     */
+    open val undoSize: Int
+        get() = undoStack.size
+
+    protected val undoStack: LinkedList<() -> Unit> = LinkedList<() -> Unit>()
+
+    /**
+     * Adds all vertices and edges in [g] to this graph without adding the necessary operations to the undo stack.
+     */
+    constructor(g: SimpleGraph<V>) : this() {
+        for (v in g.V) {
+            super.addVertex(v)
+        }
+        for ((v1, v2) in g.edgeIterator()) {
+            super.addEdge(v1, v2)
+        }
+    }
+
+    /**
+     * Reverts the last operation that has been performed on this graph. Does nothing if no operation exists that
+     * can be reverted.
+     */
+    open fun undo() {
+        if (undoStack.isNotEmpty()) {
+            undoStack.removeFirst()()
+        }
+    }
+
+    /**
+     * Clears the undo stack.
+     */
+    open fun clearStack() {
+        undoStack.clear()
+    }
+
+    override fun addVertex(v: V): Boolean {
+        if (super.addVertex(v)) {
+            undoStack.addFirst { super.removeVertex(v) }
+            return true
+        }
+        return false
+    }
+
+    override fun removeVertex(v: V): Boolean {
+        val neighborSet = if (v in super.V) {
+            super.neighbors(v).toSet()
+        } else {
+            emptySet()
+        }
+        if (super.removeVertex(v)) {
+            undoStack.addFirst {
+                addVertex(v)
+                for (n in neighborSet) {
+                    super.addEdge(v, n)
+                }
+            }
+            return true
+        }
+        return false
+    }
+
+    override fun addEdge(v1: V, v2: V): Boolean {
+        if (super.addEdge(v1, v2)) {
+            undoStack.addFirst { super.removeEdge(v1, v2) }
+            return true
+        }
+        return false
+    }
+
+    override fun removeEdge(v1: V, v2: V): Boolean {
+        if (super.removeEdge(v1, v2)) {
+            undoStack.addFirst { super.addEdge(v1, v2) }
+            return true
+        }
+        return false
+    }
+}
