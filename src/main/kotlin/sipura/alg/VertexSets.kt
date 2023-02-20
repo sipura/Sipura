ba1f352ba88fecf83e146d3aec47bd0c2204a620
Moritz Groß <hi@mgross.dev>
2023-02-20 14:13:45 +0200

    moved class KCoreDecomposition into VertexSets.kt fixed bug where k=n is allowed as a valid parameter added some tests

--- src/main/kotlin/sipura/alg/VertexSets.kt
+++ src/main/kotlin/sipura/alg/VertexSets.kt
@@ -2,6 +2,8 @@ package sipura.alg
 
 import sipura.graphs.SimpleGraph
 import sipura.utils.SetTheory.isDisjoint
+import java.util.*
+import kotlin.collections.HashSet
 
 object VertexSets {
 
@@ -101,4 +103,46 @@ object VertexSets {
      */
     fun <V> cutSize(g: SimpleGraph<V>, S: Collection<V>): Int =
         S.sumOf { s -> g.neighbors(s).count { it !in S } }
+
+    class KCoreDecomposition<V>(val g: SimpleGraph<V>) {
+
+        private val lists: Array<LinkedList<V>> = Array(g.n) { LinkedList() }
+        private val nextNonEmpty: IntArray = IntArray(g.n) { -1 }
+
+        init {
+            val degeneracyOrdering = GraphProperty.degeneracyOrdering(g).second
+            val found = HashSet<V>()
+            var k = 0
+            for (v in degeneracyOrdering) {
+                found.add(v)
+                val forwardDegree = g.neighbors(v).count { it !in found }
+                if (forwardDegree > k) {
+                    for (i in k until forwardDegree) {
+                        nextNonEmpty[i] = forwardDegree
+                    }
+                    k = forwardDegree
+                }
+                lists[k].addLast(v)
+            }
+        }
+
+        /**
+         * Calculates the vertex set of the [k]-core of [g].
+         *
+         * The [k]-core of [g] is the largest subgraph of [g] with minimum degree at least [k].
+         *
+         * Runtime: O(|kCore([k])|)  ->  Runtime only depends on the size of the result
+         * @throws IllegalArgumentException if k is smaller than 0 or larger than the size of the graph [g].
+         */
+        fun kCore(k: Int): MutableSet<V> {
+            if (k < 0 || k >= lists.size) throw IllegalArgumentException("k must be in the range from 0 to n")
+            val core = HashSet<V>()
+            var i = k
+            while (i != -1) {
+                core.addAll(lists[i])
+                i = nextNonEmpty[i]
+            }
+            return core
+        }
+    }
 }
